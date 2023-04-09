1ffee7e4f7b7ce84ca9c0f251512a94f371a05d7
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-09 10:55:09 +0530

    updating

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 #we can use locals to substitute
 
 locals {
-  DOCDB_PASS = jsondecode(data.aws_secretsmanager_secret_version.example.secret_string)["DOCDB_PASSWORD"]
-  DOCDB_USER = jsondecode(data.aws_secretsmanager_secret_version.example.secret_string)["DOCDB_USERNAME"]
+  DOCDB_PASS = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_PASSWORD"]
+  DOCDB_USER = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_USERNAME"]
 }
