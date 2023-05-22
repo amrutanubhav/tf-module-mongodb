83017163d07af47399ed1ca49adceffb8c818605
Amrut Anubhav <anubhavamrut@gmail.com>
2023-05-22 22:09:04 +0530

    updating

--- locals.tf
+++ locals.tf
@@ -3,4 +3,5 @@
 locals {
   DOCDB_PASS = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_PASSWORD"]
   DOCDB_USER = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_USERNAME"]
+  DNS_NAME   = aws_docdb_cluster.docdb.endpoint
 }
--- schema.tf
+++ schema.tf
@@ -21,7 +21,7 @@
 resource "null_resource" "docdb-schema" {
   
   # This is how we can create depenency and ensure this will only run after the creation if the RDS Instance.
-  depends_on = [aws_docdb_cluster.docdb]
+  depends_on = [aws_docdb_cluster.docdb, aws_docdb_cluster_instance.cluster_instances]
 
   provisioner "local-exec" {
         command = <<EOF
@@ -30,8 +30,8 @@ resource "null_resource" "docdb-schema" {
         curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
         unzip -o mongodb.zip
         cd mongodb-main
-        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js 
-        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js 
+        mongo --ssl --host ${local.DNS_NAME}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username ${local.DOCDB_USER} --password ${local.DOCDB_PASS} < catalogue.js 
+        mongo --ssl --host ${local.DNS_NAME}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username ${local.DOCDB_USER} --password ${local.DOCDB_PASS} < users.js 
         EOF
     }  
 }
